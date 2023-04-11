a1faf5d0e6380d3470ec7ba1c418dfd3d7e416ed
Ivan <91838070+expctr@users.noreply.github.com>
2023-04-11 12:52:56 +0300

    added IGNGNeuron interface

---
+++ ClusteringLib/IGNGNeuron.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ItemLib;
+using EuclideanGeometryLib;
+using RandomAlgoLib;
+using AlgorithmLib;
+using System.Drawing;
+using System.Threading;
+
+namespace ClusteringLib
+{
+    public interface IGNGNeuron : IClusteringNode
+    {
+        void GetComponent(List<IGNGNeuron> Component, Dictionary<IGNGNeuron, bool> UsedNeurons);
+
+        void IncreaseError(Item item);
+
+        void Learn(double[] point, double learningSpeed);
+
+        void LearnNeighbours(Item item, double learningSpeed);
+
+        void IncreaseAges();
+
+        void Connect(IGNGNeuron neuron);
+
+        void DeleteOldNeighbours(int maxAge);
+
+        bool NoNeighbours { get; }
+
+        IGNGNeuron FindMostIncorrectNeighbour();
+
+        void MultiplyError(double x);
+
+        void PartialConnect(IGNGNeuron neuron);
+
+        void PartialDisconnect(IGNGNeuron neuron);
+
+        double GetError { get; }
+
+        void Disconnect(IGNGNeuron neuron);
+    }
+}
