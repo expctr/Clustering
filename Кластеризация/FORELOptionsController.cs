d4c092fe4e060bae40a5412ab868d7b3289f999d
dontyouo <danchuodanchuo@gmail.com>
2023-04-17 17:21:48 +0300

    created FORELOptions mvc implementation

---
+++ Кластеризация/FORELOptionsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Кластеризация {
+
+    public class FORELOptionsController {
+
+        private FORELOptionsForm form;
+
+        private FORELOptionsModel model;
+
+        public FORELOptionsController(FORELOptionsForm form, FORELOptionsModel model) {
+            this.form = form;
+            this.model = model;
+        }
+        
+        public void AddEventHandlers()
+        {
+            form.Load += FORELOptionsForm_Load;
+            form.GetApplyB().Click += ApplyB_Click;
+        }
+        
+        private void FORELOptionsForm_Load(object sender, EventArgs e)
+        {
+            form.ShowOptions(model.Options);
+        }
+        
+        private void ApplyB_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                model.Options.ReachabilityRadius = double.Parse(form.GetReachabilityRadiusTB().Text);
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка. Радиус достижимости введен некорректно.");
+                return;
+            }
+            if (model.Options.ReachabilityRadius < 0)
+            {
+                MessageBox.Show("Ошибка. Радиус достижимости не может быть отрицательным.");
+                return;
+            }
+            form.parentModel.SetOptions(model.Options);
+            MessageBox.Show("Настройки сохранены.");
+        }
+
+    }
+
+}
---
+++ Кластеризация/FORELOptionsModel.cs
@@ -0,0 +1,18 @@
+﻿using ClusteringLib;
+
+namespace Кластеризация {
+
+    public class FORELOptionsModel {
+
+        FORELOptionsForm form;
+
+        public ClusteringOptions Options;
+
+        public FORELOptionsModel(FORELOptionsForm form, MainModel mainModel) {
+            this.form = form;
+            Options = mainModel.GetOptions();
+        }
+
+    }
+
+}
